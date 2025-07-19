a629ff0eecb8cc3c4c774b8d5b74266e35246bf9
Jatin Sharma <jatin0237@gmail.com>
2025-07-19 16:33:46 +0530

    created services page (#4)
    * created services page/modified Hero page

---
+++ src/assets/assets.js
@@ -0,0 +1,43 @@
+import cybersecurity_bg from "./Cybersecurity_bg.jpg";
+import cybersecurity_logo from "./Cybersecurity_logo.png";
+import dataanalytics_bg from "./Dataanalytics_bg.jpg";
+import dataanalytics_logo from "./Dataanalytics_logo.png";
+import webdevelopment_bg from "./Webdevelopment_bg.jpg";
+import webdevelopment_logo from "./Webdevelopment_logo.png";
+import digitalmarketing_bg from "./Digitalmarketing_bg.jpg";
+import automation_bg from "./automation_bg.jpg";
+import cloud_bg from "./Cloud_bg.jpg";
+import heroslider_bg from "./Heroslider_bg.jpg";
+import blackarrow from "./Blackarrow.png";
+import group_4 from "./Group 4.png";
+import mobileapp_bg from "./Mobileapp_bg.jpg";
+import nocode_bg from "./Nocode_bg.jpg";
+import logo from "./qubitnets_logo.png";
+import softwaredev_bg from "./Softwaredevelopment_bg.jpg";
+import union from "./Union.png";
+import macinelearning_bg from "./Machinelearning_bg.jpg";
+import aidevelopment_bg from "./Aidevelopment_bg.jpg";
+import pinkarrow from "./right-arrow.png";
+const assets = {
+  cybersecurity_logo,
+  cybersecurity_bg,
+  dataanalytics_logo,
+  dataanalytics_bg,
+  webdevelopment_bg,
+  webdevelopment_logo,
+  digitalmarketing_bg,
+  automation_bg,
+  cloud_bg,
+  heroslider_bg,
+  blackarrow,
+  group_4,
+  mobileapp_bg,
+  nocode_bg,
+  logo,
+  softwaredev_bg,
+  union,
+  aidevelopment_bg,
+  macinelearning_bg,
+  pinkarrow,
+};
+export default assets;
